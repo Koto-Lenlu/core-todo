673cc0e3893248a61cf3df1647d89a32e6fc9bd3
Koto-Lenlu <takeda.koto.555@gmail.com>
2020-08-22 11:46:33 +0900

    dev edit function
    ・indexメソッドの一覧表示方法改善
    ・編集機能を作成
    ・ポモドーロ機能のルート、コントローラ、Viewの下地作成

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,16 +1,28 @@
 class TodosController < ApplicationController
   before_action :authenticate_user!
-  
+  # before_action :move_to_index, only: [:create, :update, :destroy]
   def index
     @todo = Todo.new
-    @todos = Todo.order('created_at ASC')
-    todos = @todos 
-    @mytodos = current_user.todos
+    @todos = current_user.todos.order("created_at ASC")
   end
 
   def create
     Todo.create(todo_params)
     redirect_to :root
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    todo.update(todo_params)
+    redirect_to :root
+  end  
+
+  def pomodoro
+    @todo = Todo.find(params[:id])
   end
 
   def destroy
@@ -19,9 +31,14 @@ class TodosController < ApplicationController
     redirect_to :root
   end
 
+
   private
+
   def todo_params
     params.require(:todo).permit(:name, :comment, :quote).merge(user_id: current_user.id)
   end
 
+  # def move_to_index
+  #   redirect_to :root
+  # end
 end
